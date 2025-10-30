f4ff6cdb9fa544201ebb29e3478001c9c3d66721
24f2004621 <24f2004621@ds.study.iitm.ac.in>
2025-10-30 15:28:00 +0530

    nothing

--- backend/routes.py
+++ backend/routes.py
@@ -20,7 +20,7 @@ class PizzaAPI(Resource):
                 return {'message': 'Pizza not found!'}, 404
             return {'id': pizza.id, 'name': pizza.name, 'toppings': pizza.toppings}, 200
         pizzas = Pizza.query.all()
-        
+# fdfh        # 
         pizzas = [{'id': p.id, 'name': p.name, 'toppings': p.toppings} for p in pizzas]
         return {'message': 'Pizzas fetched successfully!', "pizzas": pizzas}, 200
     
