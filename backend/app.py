13b0c66a65f79848399ef36937773a5e6e7db0ac
24f2004621 <24f2004621@ds.study.iitm.ac.in>
2025-10-30 15:15:18 +0530

    hello

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# connecting to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+from models import db
+db.init_app(app)
+
+# connecting to routes
+from routes import api
+api.init_app(app)
+
+if __name__ == '__main__':
+    
+    with app.app_context():
+        db.create_all()  # Create database tables for our data models
+
+    app.run(debug=True)
